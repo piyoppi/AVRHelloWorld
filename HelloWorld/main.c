7be9eff24855258f41caba960c8c0b26a36d4dc6
piyoppi <garakuta.toolbox@gmail.com>
2019-12-31 01:10:26 +0900

    LEDがながれるようにしてみる

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -8,11 +8,9 @@
 volatile unsigned int cnt = 0;
 
 ISR(TIMER0_OVF_vect) {
-  if(cnt == 255){
-    cnt=0;
-    PORTB=cnt;
-  } else {
-    PORTB=cnt++;
+  PORTB = PORTB >> 1;
+  if( PORTB == 0 ) {
+    PORTB = 0b10000000;
   }
 }
 
@@ -20,8 +18,9 @@ void interrupt_test() {
   DDRB=0xFF;
   DDRD=0x00;
   OCR0=0xFF;
-  TCCR0 = 0b00000101;     // TIMER0設定（分周 = 1024)
-  TIMSK |= 0b00000010;    // TIMER0オーバーフロー割り込み許可
+  PORTB=0b10000000;
+  TCCR0 = 0b00000101;     // TIMER0設定（�E周 = 1024)
+  TIMSK |= 0b00000010;    // TIMER0オーバ�Eフロー割り込み許可
   sei();
   while (1) 
   {
