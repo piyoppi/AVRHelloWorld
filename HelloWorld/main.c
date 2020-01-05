4eb087c73e1c88032f47c8b4916cc79e227eb695
piyoppi <garakuta.toolbox@gmail.com>
2020-01-05 14:59:31 +0900

    コンパレータをつかってみる

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -8,25 +8,40 @@
 volatile unsigned int cnt = 0;
 
 ISR(TIMER0_OVF_vect) {
-  PORTB = PORTB >> 1;
-  if( PORTB == 0 ) {
-    PORTB = 0b10000000;
+  PORTC = PORTC >> 1;
+  if( PORTC == 0 ) {
+    PORTC = 0b10000000;
+  }
+}
+
+ISR(ANA_COMP_vect) {
+  PORTC = PORTC >> 1;
+  if( PORTC == 0 ) {
+    PORTC = 0b10000000;
   }
 }
 
 void interrupt_test() {
-  DDRB=0xFF;
-  DDRD=0x00;
-  OCR0=0xFF;
-  PORTB=0b10000000;
+  PORTC=0b10000000;
   TCCR0 = 0b00000101;     // TIMER0 divider = 1024
   TIMSK |= 0b00000010;    // TIMER0 interrupt settings
+  OCR0=0xFF;
   sei();
   while (1) 
   {
   }
 }
 
+void comp_test() {
+  ACSR = 0b01001010;
+  PORTC = 0b10101010;
+  sei();
+  while(1) {}
+}
+
 int main(void) {
-  interrupt_test();
+  DDRB=0x00;
+  DDRC=0xFF;
+  DDRD=0x00;
+  comp_test();
 }
